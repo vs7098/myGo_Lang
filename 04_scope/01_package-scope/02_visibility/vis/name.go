197628355db3a031b22fd7b566e4222bf35412eb
Vinay Singh <vs7098@vinays-mbp.lan>
2017-12-24 13:39:55 -0600

    04_scope_updatated

--- 04_scope/01_package-scope/02_visibility/vis/name.go
+++ 04_scope/01_package-scope/02_visibility/vis/name.go
@@ -1,5 +1,5 @@
 package vis
 
 // MyName is exported because it starts with a capital letter
-var MyName = "Todd"
+var MyName = "VinaY"
 var yourName = "Future Rock Star Programmer"
--- 04_scope/02_block-scope/01_this-does-not-compile/main.go
+++ 04_scope/02_block-scope/01_this-does-not-compile/main.go
@@ -10,6 +10,7 @@ func main() {
 
 func foo() {
 	// no access to x
-	// this does not compile
+	// this does not compile\
+	x :=100
 	fmt.Println(x)
 }
